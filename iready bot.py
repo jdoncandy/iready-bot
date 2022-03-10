63eaa528357c4f26c3c1d8fba891b01d37a24bc4
jdoncandy <101303570+jdoncandy@users.noreply.github.com>
2022-03-09 20:58:23 -0500

    Add files via upload

---
+++ iready bot.py
@@ -0,0 +1,30 @@
+import pyautogui
+import time
+
+time.sleep(5)
+
+#var = pyautogui.locateCenterOnScreen('text.png')
+
+#pyautogui.click(var)
+
+#print("sup")
+
+def bot(img):
+    var = pyautogui.locateCenterOnScreen(img)
+
+    if var == None:
+        onscreen = False
+    else:
+        onscreen = True
+    print(var)
+    return onscreen
+
+
+
+
+if bot('text.png') == True:
+    print("lets go")
+
+else:
+    print("not on screen")
+    
